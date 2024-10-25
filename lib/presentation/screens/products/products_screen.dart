442b1c32bf7f11d5604e777587e76b68de25410f
SebiPochi <sebipochi@gmail.com>
2024-10-25 01:49:49 -0300

    Cambio para visualizar productos desde Firebase (y no local)

--- lib/presentation/screens/products/products_screen.dart
+++ lib/presentation/screens/products/products_screen.dart
@@ -1,33 +1,73 @@
-import 'package:appnike/database/products/products.database.dart';
 import 'package:appnike/domain/products/product.dart';
 import 'package:appnike/presentation/components/products/product_card.dart';
+import 'package:appnike/services/product_service.dart';
 import 'package:flutter/material.dart';
 
-class ProductsScreen extends StatelessWidget {
+class ProductsScreen extends StatefulWidget {
   static const String name = 'products';
-  ProductsScreen({super.key});
-  final List<Product> products = productsDB;
+  const ProductsScreen({super.key});
+
+  @override
+  State<ProductsScreen> createState() => _ProductsScreenState();
+}
+
+class _ProductsScreenState extends State<ProductsScreen> {
+  final ProductService _productService = ProductService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Products Screen'),
+      body: Center(
+        child: _ProductList(),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          childAspectRatio: 1.5,
-        ),
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return ProductCard(product: product);
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _productService.addProduct(
+            Product(
+              id: 5,
+              name: 'Adidas Yeezy Boost 350',
+              description: 'Zapatillas de edición limitada diseñadas por Kanye West.',
+              type: 'Streetwear',
+              price: 399.99,
+              image:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRv45TDdXDVPDiCctILmVGIMO9cwvJWA1_Fg&s',
+              isNew: false,
+            )
+          );
         },
+        child: const Icon(Icons.add)
       ),
+    );
+  }
+
+  Widget _ProductList() {
+    return StreamBuilder(
+      stream: _productService.getAllProducts(), 
+      builder: (context, snapshot) {
+        List products = snapshot.data?.docs ?? [];
+        return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // number of items in each row
+            mainAxisSpacing: 2.0, // spacing between rows
+            crossAxisSpacing: 2.0, // spacing between columns
+          ),
+          itemCount: products.length,
+          itemBuilder: (context, index) { 
+            Product product = products[index].data();
+            // return Text(product.name);
+            return ProductCard(product: product);
+          }
+        );
+        
+        // return ListView.builder(
+        //   itemCount: products.length,
+        //   itemBuilder: (context, index) {
+        //     Product product = products[index].data();
+        //     // return Text(product.name);
+        //     return ProductCard(product: product);
+        //   }
+        // );
+      }
     );
   }
 }
