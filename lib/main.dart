1fb170360a538561a40aa7178d61878a07a814c8
Sebastian <sebipochi@gmail.com>
2024-10-30 19:48:27 -0300

    Borro el "debug banner"

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class MainApp extends StatelessWidget {
       ],
       child: MaterialApp.router(
         routerConfig: appRouter,
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
