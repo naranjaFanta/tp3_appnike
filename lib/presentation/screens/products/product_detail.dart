b0277dde50e8c1f02b6833ee46bbc4ba9ab22273
SebiPochi <sebipochi@gmail.com>
2024-11-06 20:25:25 -0300

    Arreglo para que product_detail sea scrolleable

--- lib/presentation/screens/products/product_detail.dart
+++ lib/presentation/screens/products/product_detail.dart
@@ -22,55 +22,60 @@ class ProductDetail extends StatelessWidget {
               appBar: AppBar(
                 title: Text(snapshot.data!.name),
               ),
-              body: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Image.network(snapshot.data!.image),
-                    ),
-                    const SizedBox(height: 16),
-                    if ((snapshot.data!.isNew ?? false))
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 8),
-                        child: Text(
-                          'Nuevo',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 215, 195, 104),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+              body: 
+                  ListView(
+                    shrinkWrap: true,
+                      children: [
+                        Center(
+                          child: Image.network(snapshot.data!.image),
+                        ),
+                        const SizedBox(height: 16),
+                        if ((snapshot.data!.isNew ?? false))
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 8),
+                            child: Center(
+                              child: Text(
+                                'Nuevo',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 215, 195, 104),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                        Center(
+                          child: Text(snapshot.data!.name,
+                              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                        ),
+                        const SizedBox(height: 8),
+                        Center(child: Text(snapshot.data!.description)),
+                        const SizedBox(height: 16),
+                        Center(
+                          child: Text('Precio: \$${snapshot.data!.price}',
+                              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          width: MediaQuery.sizeOf(context).width,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Provider.of<CartProvider>(context, listen: false)
+                                  .addToCart(snapshot.data!);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Producto agregado al carrito')),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                            ),
+                            child: const Text('Agregar al carrito'),
                           ),
                         ),
-                      ),
-                    Text(snapshot.data!.name,
-                        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 8),
-                    Text(snapshot.data!.description),
-                    const SizedBox(height: 16),
-                    Text('Precio: \$${snapshot.data!.price}',
-                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                    const Spacer(),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Provider.of<CartProvider>(context, listen: false)
-                              .addToCart(snapshot.data!);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Producto agregado al carrito')),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                        ),
-                        child: const Text('Agregar al carrito'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
+                      ],
+                  ),
+
+          );
         } else {
           return Scaffold(
             appBar: AppBar(
