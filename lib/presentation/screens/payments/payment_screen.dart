6876f4d293b7dbc8025d66169f96a4ed9ddcd161
Gonza-Oto <gonzaoto31@gmail.com>
2024-11-08 06:34:02 -0300

    creacion de historial de compras
    clase purchase nueva
    base de datos de purchase
    provider de purchase
    PaymentScreen ahora suma a la db el purchase, el timer usa un metodo para pasar de screen
    view nueva para que el usuario vea el historial de compras
    
    Agregue lo que se habia borrado de cupones, ver bien si funciona.

--- lib/presentation/screens/payments/payment_screen.dart
+++ lib/presentation/screens/payments/payment_screen.dart
@@ -1,42 +1,112 @@
 import 'dart:async';
 import 'package:appnike/presentation/providers/cart_provider.dart';
-import 'package:appnike/presentation/screens/payments/payment_confirmation_screen.dart';
-import 'package:appnike/presentation/screens/products/products_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:appnike/presentation/providers/purchase_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:appnike/presentation/screens/payments/payment_confirmation_screen.dart';
+import 'package:appnike/database/purchases/purchases.database.dart';
+import 'package:uuid/uuid.dart'; // nos deja crear un id unico
+
 
 class PaymentScreen extends StatefulWidget {
   static const String name = 'payment_process';
   const PaymentScreen({super.key});
 
+  
   @override
-  State<PaymentScreen> createState() => _PaymentScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    
 
-class _PaymentScreenState extends State<PaymentScreen> {
+    void applyCoupon() {
+    String enteredCode = _couponController.text;
+    Coupon? coupon = couponDB.firstWhere(
+      (c) => c.code == enteredCode && !c.isUsed,
+      orElse: () => null,
+    );
 
-  @override
-  void initState() {
-    super.initState();
-    // Temporizador de 3 segundos para simular el procesamiento del pago
-    Timer(const Duration(seconds: 3), () {
-      final _cartProvider = context.read<CartProvider>();
-      _cartProvider.emptyCart();
-      context.go('/payment_confirmation');
-    });
+    if (coupon != null) {
+      setState(() {
+        double discount = (coupon.discountPercent / 100) * originalTotalAmount;
+        discountedTotalAmount = originalTotalAmount - discount.toInt();
+        coupon.isUsed = true; // Marca como usado o no
+        message = "Cupon de ${coupon.discountPercent}% aplicado.";
+      });
+    } else {
+      setState(() {
+        message = "Cupon usado o invalido.";
+      });
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: 
-          Text(
-            'Procesando el pago...',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          )
+    void processPayment() {
+      final purchaseProvider = ref.read(purchaseProvider.notifier);
+      final cartProvider = ref.read(cartProvider);
+      List<String> items = cartProvider.items.map((item) => item.name).toList(); //["Nike Air Force 1", "Adidas Ultraboost"];
+      double totalAmount = cartProvider.totalAmount;
+
+      final newPurchase = Purchase(
+        id: const Uuid().v4(),
+        date: DateTime.now(),
+        items: items,
+        totalAmount: totalAmount,
+      );
+
+      purchaseDB.add(newPurchase);
+      purchaseProvider.addPurchase(items, totalAmount);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PaymentConfirmationScreen(
+            userEmail: userEmail,
+          ),
+        ),
+      );
+    }
+
+    Timer(const Duration(seconds: 3), processPayment);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Procesando el Pago'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text(
+              'Procesando el pago...',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text("Total original: \$${originalTotalAmount}"),
+          Text("Total con descuento: \$${discountedTotalAmount}"),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              controller: _couponController,
+              decoration: InputDecoration(
+                labelText: "Ingresar Cupon",
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.check),
+                  onPressed: applyCoupon,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(message, style: TextStyle(color: Colors.green)),
+        ],
       ),
     );
   }
 }
+
+
+
+
+    
+
